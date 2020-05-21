a64c653880929f5629212c95f7f8efb804d28d15
Jihoon Park <bartleby2718@gmail.com>
2020-05-21 17:10:43 +0900

    Create fixtures with dumpdata Fix #1

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,4 +4,5 @@ pip install mod-wsgi==4.7.1
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
+python manage.py loaddata fixtures.json
 mod_wsgi-express start-server --working-directory . --reload-on-changes backend/wsgi.py --user www-data --group www-data
