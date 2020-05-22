0ec3b9fb9739c43a65e570a00b7159707ca25c7b
Jihoon Park <bartleby2718@gmail.com>
2020-05-22 09:53:30 +0900

    Delete category on button click Fix #5

--- frontend/src/Category.js
+++ frontend/src/Category.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 class Category extends React.Component {
   constructor(props) {
@@ -9,12 +10,26 @@ class Category extends React.Component {
     };
   }
 
+  handleDelete = (id) => {
+    fetch(`http://localhost:8000/api/categories/${id}/`, {
+      method: "DELETE",
+    }).then(window.location.reload());
+  };
+
   render() {
     const id = this.state.id;
     const name = this.state.name;
     return (
       <div>
         Category ID {id}: {name}
+        <Button
+          variant="danger"
+          onClick={(e) => {
+            this.handleDelete(id);
+          }}
+        >
+          Delete
+        </Button>
       </div>
     );
   }
