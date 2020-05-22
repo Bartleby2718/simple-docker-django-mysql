1bd27cba8afc06c42b305275f04ac18c9d88a1c4
Jihoon Park <bartleby2718@gmail.com>
2020-05-22 18:51:50 +0900

    Put everything into App and add a Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Category from "./Category";
+import CategoryCreateForm from "./CategoryCreateForm";
+import CategoryList from "./CategoryList";
+import Post from "./Post";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/categories">
+            <CategoryList />
+          </Route>
+          <Route path="/">
+            <CategoryList></CategoryList>
+            <CategoryCreateForm />
+            <Category id={1} />
+            <Category id={2} />
+            <Post id={1} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
