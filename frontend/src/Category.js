3a64c188f5e372be1ea8acac42f87f52b11c8bae
Jihoon Park <bartleby2718@gmail.com>
2020-05-23 14:15:34 +0900

    CategoryUpdateModal doesn't show the current name Fix #22

--- frontend/src/Category.js
+++ frontend/src/Category.js
@@ -9,7 +9,7 @@ class Category extends React.Component {
     super(props);
     this.state = {
       id: props.id,
-      name: "",
+      name: props.name,
       showingModal: false,
     };
   }
