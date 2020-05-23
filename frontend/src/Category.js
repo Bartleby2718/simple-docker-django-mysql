4e197b7c77aff83fa30f96062faaed70b83dc791
Jihoon Park <bartleby2718@gmail.com>
2020-05-23 21:27:45 +0900

    Replace update/delete buttons with a dropdown Fix #16

--- frontend/src/Category.js
+++ frontend/src/Category.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Link } from "react-router-dom";
 import CategoryUpdateModal from "./CategoryUpdateModal";
@@ -47,18 +49,21 @@ class Category extends React.Component {
     const id = this.state.id;
     const name = this.state.name;
     return (
-      <ListGroup.Item style={{ width: "40rem" }}>
+      <ListGroup.Item style={{ width: "30rem" }}>
         <Link to={`/categories/${id}`}>
-          <Button variant="Light" style={{ width: "20rem" }}>
+          <Button variant="Light" style={{ width: "25rem" }}>
             <h5>{name}</h5>
           </Button>
         </Link>
-        <Button variant="info" onClick={this.handleShow}>
-          Update name
-        </Button>
-        <Button variant="danger" onClick={this.handleDelete}>
-          Delete
-        </Button>
+
+        <DropdownButton
+          variant="outline-primary"
+          style={{ display: "inline-block" }}
+        >
+          <Dropdown.Item onClick={this.handleShow}>Rename</Dropdown.Item>
+          <Dropdown.Item onClick={this.handleDelete}>Delete</Dropdown.Item>
+        </DropdownButton>
+
         <CategoryUpdateModal
           id={id}
           name={name}
