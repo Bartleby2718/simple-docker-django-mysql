faa7cc44da30bcb087faec6ac73e0a97b3f51f8e
Jihoon Park <bartleby2718@gmail.com>
2020-05-24 14:05:19 +0900

    Sort objects by ID in responses Fix #35

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -4,13 +4,22 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        ordering = ['id']
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['id']
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.TextField()
+
+    class Meta:
+        ordering = ['id']
