2e2932724a90edd8881c7e722fbe08aa4da28f57
Jihoon Park <bartleby2718@gmail.com>
2020-05-26 23:57:56 +0900

    Login not working Fix #42

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -12,9 +12,17 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserPrivateSerializer(serializers.ModelSerializer):
+    """User serializer for private use. Shouldn't be used to reset password."""
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class CategorySerializer(serializers.ModelSerializer):
